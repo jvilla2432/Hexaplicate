ed8a22d361a4f1dad95677b67b6d9a1563dcff99
Justin Villa <lelcetz@gmail.com>
2024-06-17 00:44:00 -0400

    Added skeleton implementations

--- UI/UICLickedState.cs
+++ UI/UICLickedState.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
 
 namespace Hexaplicate.UI
 {
-    internal class UICLickedState
+    internal class UIClickedState : UIManagerState
     {
+        void UIManagerState.handle_input(MouseState state)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- UI/UIWaitingState.cs
+++ UI/UIWaitingState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Hexaplicate.UI
 {
     internal class UIWaitingState : UIManagerState
     {
+        void UIManagerState.handle_input(MouseState state)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
